9673011a957347ad24470d093001c4d8de230c8c
SimonHauguel <simon.hauguel@student.junia.com>
2022-02-23 08:19:53 +0100

    Add minimalist forms

--- back/main.js
+++ back/main.js
@@ -1,11 +1,29 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path')
 
+const withDir = rest => __dirname + rest;
+const pure = rest => path.resolve(withDir(rest))
+
+app.use(express.static(withDir('/../front/')));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/../front/index.html')
+    res.sendFile(pure('/../front/index.html'));
+});
+
+app.get('/submit', (req, res) => {
+    res.sendFile(pure('/../front/jeu.html'))
+    console.log(req.query.username)
+})
+
+io.on('connection', socket => {
+    console.log('An user connected');
 });
 
 http.listen(4200, () => {
-    console.log('Serveur Run')
-})
+    console.log('Serveur Run');
+});
+
+
