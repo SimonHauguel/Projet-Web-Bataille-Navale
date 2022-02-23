4507b35390bbdd66448008882605affdad825dbe
clementgorse <72432474+clementgorse@users.noreply.github.com>
2022-02-23 09:33:40 +0100

    ajout tableau

---
+++ front/js/createTab.js
@@ -0,0 +1,22 @@
+// tableau bataille naval
+
+let table = document.createElement('table');
+
+
+// tr.appendChild(td);
+// table.appendChild(tr);
+
+
+
+for (let i = 0; i < 10; i++) {
+    let tr = document.createElement('tr');
+    table.appendChild(tr);
+    for (let j = 0; j < 10; j++) {
+        let td = document.createElement('td');
+        tr.appendChild(td);
+        td.textContent = 'cul';
+    }
+}
+
+
+document.getElementById('body').appendChild(table);
