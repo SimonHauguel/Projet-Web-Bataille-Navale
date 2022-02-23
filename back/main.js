99f82bb1db72877856b807853247ba786545188c
SimonHauguel <simon.hauguel@student.junia.com>
2022-02-23 09:03:30 +0100

    Forms

--- back/main.js
+++ back/main.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const path = require('path')
+const app     = express();
+const http    = require('http').createServer(app);
+const io      = require('socket.io')(http);
+const path    = require('path');
+const sql     = require('my-sql');
+
 
 const withDir = rest => __dirname + rest;
-const pure = rest => path.resolve(withDir(rest))
+const pure = rest => path.resolve(withDir(rest));
 
 app.use(express.static(withDir('/../front/')));
 
@@ -14,16 +16,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/submit', (req, res) => {
-    res.sendFile(pure('/../front/jeu.html'))
-    console.log(req.query.username)
-})
+    res.sendFile(pure('/../front/jeu.html'));
+    console.log(req.query.username);
+    console.log(req.query.password);
+});
 
 io.on('connection', socket => {
     console.log('An user connected');
 });
 
 http.listen(4200, () => {
-    console.log('Serveur Run');
+    console.log('Serveur Run on Port 4200');
 });
 
 
