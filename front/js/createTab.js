5fb5877696a9440e965546e3ac4082cd96b102dc
Uriel MILLOGO <amillogo16@gmail.com>
2022-03-01 19:51:29 +0100

    branch main

--- front/js/createTab.js
+++ front/js/createTab.js
@@ -1,23 +1,21 @@
-// tableau bataille naval
+/* ----------------tableau bataille naval--------------- */
+/*-------------------------------------------------------*/
 
+// creation de la grille
 let table = document.createElement('table');
+// taille de la grille
+let tabSize = 10;
 
+for (let i = 0; i < tabSize; i++) {
 
-// tr.appendChild(td);
-// table.appendChild(tr);
-
-
-
-for (let i = 0; i < 10; i++) {
     let tr = document.createElement('tr');
     table.appendChild(tr);
-    for (let j = 0; j < 10; j++) {
+
+    for (let j = 0; j < tabSize; j++) {
+
         let td = document.createElement('td');
         tr.appendChild(td);
-        //td.textContent = 'cul';
-        td.addEventListener('click',event_plateau);
+        td.addEventListener('click', event_plateau);
     }
 }
-
-
 document.getElementById('body').appendChild(table);
