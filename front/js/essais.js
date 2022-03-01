7bc07b8cb076c3bc5ebc45f4198f13aec3e3ddba
Uriel MILLOGO <amillogo16@gmail.com>
2022-03-01 23:53:33 +0100

    numerotation des cases de la grille du jeu

---
+++ front/js/essais.js
@@ -0,0 +1,25 @@
+/* ----------------tableau bataille naval--------------- */
+/*-------------------------------------------------------*/
+
+// creation de la grille
+let table = document.createElement('table');
+// taille de la grille
+let tabSize = 10;
+// numérotation des cases
+let numberCase = 0;
+
+for (let i = 0; i < tabSize; i++) {
+
+    let tr = document.createElement('tr');
+    table.appendChild(tr);
+
+    for (let j = 0; j < tabSize; j++) {
+
+        let td = document.createElement('td');
+        tr.appendChild(td);
+        td.addEventListener('click', event_plateau);
+        td.setAttribute("id",numberCase);
+        numberCase++;
+    }
+}
+document.getElementById('body').appendChild(table);
