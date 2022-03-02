c0a6d2371596f4b34e97a939f69f06403ce190ba
clementgorse <72432474+clementgorse@users.noreply.github.com>
2022-03-02 08:29:45 +0100

    ajout armes

--- front/js/armes.js
+++ front/js/armes.js
@@ -1,1 +1,16 @@
-//affichage des armes
+/*-------------tableau des armes--------------*/
+
+let weaponTable = document.createElement('table');
+let trWeaponTable = document.createElement('tr');
+// marqueur des cases
+let attributeTable = ["radar","bombe","torpille","missile"];
+//nombre d'armes
+let numberOfWeapons = 4;
+
+for (let i = 0; i < numberOfWeapons; i++){
+    let tdWeaponTable = document.createElement('td');
+    tdWeaponTable.setAttribute("id",attributeTable[i]);
+    trWeaponTable.appendChild(tdWeaponTable);
+}
+weaponTable.appendChild(trWeaponTable);
+document.getElementById('body').appendChild(weaponTable);
