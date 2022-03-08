63cedfc257528d42881337b0d229fce11b6f527f
Uriel MILLOGO <amillogo16@gmail.com>
2022-03-08 16:07:00 +0100

    ajout du friendly, tableau des armes in mid

--- front/js/weapons.js
+++ front/js/weapons.js
@@ -1,8 +1,8 @@
 /*-------------tableau des armes--------------*/
-
+/*-------------------------------------------- */
 let weaponTable = document.createElement('table');
 let trWeaponTable = document.createElement('tr');
-// marqueur des cases
+// tableau contenant les id des cases du tableau des armes
 let attributeTable = ["radar","bombe","torpille","missile"];
 //nombre d'armes
 let numberOfWeapons = 4;
@@ -15,4 +15,21 @@ for (let i = 0; i < numberOfWeapons; i++){
 }
 weaponTable.appendChild(trWeaponTable);
 document.getElementById('weapon').appendChild(weaponTable);
+/*-----------------------apllication des images-----------------------*/
+let radar = document.getElementById("radar");
+radar.style.backgroundImage = "url(img/weapons/radar.png)";
 
+let bombe = document.getElementById("bombe");
+bombe.style.backgroundImage = "url(img/weapons/bombe_a_fragment.png)";
+
+let torpille = document.getElementById("torpille");
+torpille.style.backgroundImage = "url(img/weapons/torpille.png)";
+
+let missile = document.getElementById("missile");
+missile.style.backgroundImage = "url(img/weapons/missile.png)";
+/**/
+for(let attribut of attributeTable){
+    document.getElementById(attribut).style.backgroundSize = "contain";
+    document.getElementById(attribut).style.backgroundRepeat = "no-repeat";
+    document.getElementById(attribut).style.backgroundPosition = "center";
+}
