6b3430eb0a66581c2356c8413a8aac60bd5be100
clementgorse <72432474+clementgorse@users.noreply.github.com>
2022-03-27 18:30:22 +0200

    ajout events des ombres pour les bateaux

--- front/js/opposingGrid.js
+++ front/js/opposingGrid.js
@@ -17,3 +17,7 @@ for(let oneCase of casesOfGrid){
 let friendlyGrid = new Grid(10,0);
 friendlyGrid = friendlyGrid.generate();
 document.getElementById('friendly').appendChild(friendlyGrid);
+for(let i =100; i < 200; i++){
+    casesOfGrid[i].addEventListener('mouseover', event_ombre_bateau);
+    casesOfGrid[i].addEventListener('mouseleave', event_ombre_bateau_fin);
+}
